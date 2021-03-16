4255f2e10fd33863fe8b44663565d33e1ea0fc96
M4SHR0 <fall.est.mori@gmail.com>
2021-03-17 03:41:54 +0900

    昨日の追加とREADMEの更新
    自分の投稿のみ表示・削除、いいね・RTの実装
    それに伴う選択メニューとREADMEの更新

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ TwitterClient that runs on the CLI
 prototypeTwitterClient(https://github.com/M4SHR0/prototypeTwitterClient)の後継
 
 # 実装予定
-- ツイート
-- ツイートの削除
-- ツイートへのいいね
-- RT
-- 自分の投稿の表示
-- タイムラインの表示(Streamingではない)
 - タイムラインの表示(Streaming)
 - アカウントの切り替え
 - アカウント情報の保存
 
 # 実装済み
+- ツイート
+- タイムラインの表示(Streamingではない)
+- 自分の投稿の表示
+- ツイートの削除
+- ツイートへのいいね
+- RT
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def tweet():
     elif len(text)==0:
         print("未入力です.最初からやり直してください")
     else:
-        print("完了")
+        print("投稿完了")
         api.update_status(text)
 
 #タイムラインの表示(20件)
@@ -35,18 +35,79 @@ def display_home_20tweet():
         print("投稿日時{}\tクライアント名:{}".format(get_tweet.created_at,get_tweet.source))
         print("----------------------------------------------------------------------")
 
+#自身のツイートのみ表示
+def display_owned_tweet_only():
+    print("----------------------------------------------------------------------")
+    for get_tweet in api.user_timeline(api.me().id):
+        print("Tweeted by:{}\tTweetID:{}".format(get_tweet.user.screen_name,get_tweet.id))
+        print("{}".format(get_tweet.text))
+        print("いいね:{}\tRT:{}".format(get_tweet.favorite_count,get_tweet.retweet_count))
+        print("投稿日時{}\tクライアント名:{}".format(get_tweet.created_at,get_tweet.source))
+        print("----------------------------------------------------------------------")
+
+#選択した自身のツイートを削除
+def deleate_owned_tweet():
+    select_id=input("削除する自身のツイートのidを入力してください")
+    try:
+        api.destroy_status(select_id)
+        print("削除完了")
+    except:
+        print("無効なid")
+
+#選択したツイートをいいねする
+def like_tweet():
+    select_id=input("いいねするツイートのidを入力してください")
+    try:
+        api.create_favorite(select_id)
+        print("いいね完了")
+    except:
+        print("無効なid")
+
+#選択したツイートをRT
+def RT_tweet():
+    select_id=input("RTするツイートのidを入力してください")
+    try:
+        api.retweet(select_id)
+        print("RT完了")
+    except:
+        print("無効なid")
+
 #main
 if __name__=='__main__':
     while(True):
         print("選択メニュー")
-        select_num=input("1:ツイート\t2:TLから最新20件の投稿を表示\t3:終了\n")
+        select_num=input("1:ツイートの投稿\t2:TLから最新20件の投稿を表示・いいね・RT\n3:自身のツイートのみ20件表示・削除\t4:終了\n")
         if select_num=='1' or select_num=='１':
-            print("ツイート")
+            print("ツイートの投稿")
             tweet()
         elif select_num=='2' or select_num=='２':
-            print("TLから最新20件の投稿を表示")
+            print("TLから最新20件の投稿を表示・いいね・RT")
             display_home_20tweet()
+            print("いいね・RTをする？")
+            select_num=input("1:いいね\t2:RT\t3:メニューに戻る\n")
+            if select_num=='1' or select_num=='１':
+                print("いいね")
+                like_tweet()
+            elif select_num=='2' or select_num=='２':
+                print("RT")
+                RT_tweet()
+            elif select_num=='3' or select_num=='３':
+                print("メニューに戻る")
+            else:
+                print("無効な入力\n最初からやり直してください")
         elif select_num=='3' or select_num=='３':
+            print("自身のツイートのみ20件表示・削除")
+            display_owned_tweet_only()
+            print("削除をする？")
+            select_num=input("1:削除\t2:メニューに戻る\n")
+            if select_num=='1' or select_num=='１':
+                print("削除")
+                deleate_owned_tweet()
+            elif select_num=='2' or select_num=='２':
+                print("メニューに戻る")
+            else:
+                print("無効な入力\n最初からやり直してください")
+        elif select_num=='4' or select_num=='４':
             print("終了")
             exit()
         else:
